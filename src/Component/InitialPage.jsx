23d1ef39d83ad4837d440c1fe3a8ad904d85917b
26kumar <rupesh583k@gmail.com>
2025-02-06 20:56:16 +0530

    Update InitialPage.jsx

--- src/Component/InitialPage.jsx
+++ src/Component/InitialPage.jsx
@@ -7,7 +7,9 @@ const InitialPage = () => {
   useEffect(() => {
     const fetchApi = async () => {
       try {
-        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
+        const response = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/categories.php`
+        );
         const menuJson = await response.json();
         setMenu(menuJson.categories || []);
       } catch (error) {
@@ -20,21 +22,33 @@ const InitialPage = () => {
 
   return (
     <>
-    <h3 className="category">Here are few categories our site contains :- </h3>
+      <h3 className="category" style={{ marginBottom: "0px" , fontSize: "30px"}}>
+        Here are a few categories our site contains:
+      </h3>
+      <h4 style={{ marginTop: "0px" }}>
+        To get a recipe, enter the dish name in the search box.
+      </h4>
 
-    <div className='meals'>
-      {menu.length > 1 ? (
-        menu.slice(1).map((curItem) => (
-          <div className='mealImg' key={curItem.idCategory}>
-            <img src={curItem.strCategoryThumb} alt={curItem.strCategory} />
-            <p>{curItem.strCategory}</p>
-          </div>
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  </>
+      <div className="meals">
+        {menu.length > 1 ? (
+          menu.slice(1).map((curItem) => (
+            <div
+              className="mealImg"
+              key={curItem.idCategory}
+              style={{ pointerEvents: "none" }}
+            >
+              <img src={curItem.strCategoryThumb} alt={curItem.strCategory} />
+              <p> </p>
+              <button style={{ pointerEvents: "none" }}>
+                {curItem.strCategory}
+              </button>
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </>
   );
 };
 
